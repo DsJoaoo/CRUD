d15cfc46784e8604d1a296d6f5c4d7fe4ce3851a
João Paulo <joaopaulo.eu29@hotmail.com>
2023-06-16 11:39:47 -0300

    Editando diretamente na tabela

--- script.js
+++ script.js
@@ -1,77 +1,96 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.getElementById("meuFormulario");
-    const tabela = document.getElementById("minhaTabela");
-    const botaoEditar = document.getElementById("botaoEditar");
-    const botaoExcluir = document.getElementById("botaoExcluir");
-    let linhaSelecionada = null;
-  
-    tabela.addEventListener("click", function (evento) {
-      if (evento.target.tagName === "TD") {
-        const linhaAtual = evento.target.parentNode;
-  
-        if (linhaSelecionada) {
-          linhaSelecionada.classList.remove("selecionado");
-        }
-  
-        linhaSelecionada = linhaAtual;
-        linhaSelecionada.classList.add("selecionado");
-  
-        botaoEditar.removeAttribute("disabled");
-        botaoExcluir.removeAttribute("disabled");
-      }
-    });
-  
-    formulario.addEventListener("submit", function (evento) {
-      evento.preventDefault();
-  
-      const nome = document.getElementById("nome").value;
-      const email = document.getElementById("email").value;
-      const telefone = document.getElementById("telefone").value;
-  
+  const tabela = document.getElementById("minhaTabela");
+  const botaoEditar = document.getElementById("botaoEditar");
+  const botaoExcluir = document.getElementById("botaoExcluir");
+  let linhaSelecionada = null;
+
+  tabela.addEventListener("click", function (evento) {
+    if (evento.target.tagName === "TD") {
+      const celulaAtual = evento.target;
+
       if (linhaSelecionada) {
-        linhaSelecionada.cells[0].textContent = nome;
-        linhaSelecionada.cells[1].textContent = email;
-        linhaSelecionada.cells[2].textContent = telefone;
-  
         linhaSelecionada.classList.remove("selecionado");
-        linhaSelecionada = null;
-  
-        botaoEditar.setAttribute("disabled", "disabled");
-        botaoExcluir.setAttribute("disabled", "disabled");
-      } else {
-        const novaLinha = tabela.insertRow();
-        const colunaNome = novaLinha.insertCell();
-        const colunaEmail = novaLinha.insertCell();
-        const colunaTelefone = novaLinha.insertCell();
-  
-        colunaNome.textContent = nome;
-        colunaEmail.textContent = email;
-        colunaTelefone.textContent = telefone;
       }
-  
-      formulario.reset();
-    });
-  
-    botaoEditar.addEventListener("click", function () {
-      if (linhaSelecionada) {
-        const nome = linhaSelecionada.cells[0].textContent;
-        const email = linhaSelecionada.cells[1].textContent;
-        const telefone = linhaSelecionada.cells[2].textContent;
-  
-        document.getElementById("nome").value = nome;
-        document.getElementById("email").value = email;
-        document.getElementById("telefone").value = telefone;
-      }
-    });
-  
-    botaoExcluir.addEventListener("click", function () {
-      if (linhaSelecionada) {
-        tabela.deleteRow(linhaSelecionada.rowIndex);
-        linhaSelecionada = null;
-  
-        botaoEditar.setAttribute("disabled", "disabled");
-        botaoExcluir.setAttribute("disabled", "disabled");
-      }
-    });
+
+      linhaSelecionada = celulaAtual.parentNode;
+      linhaSelecionada.classList.add("selecionado");
+
+      botaoEditar.removeAttribute("disabled");
+      botaoExcluir.removeAttribute("disabled");
+    }
   });
-  
+
+  botaoEditar.addEventListener("click", function () {
+    if (linhaSelecionada) {
+      const colunas = linhaSelecionada.cells;
+      const nome = colunas[0];
+      const email = colunas[1];
+      const telefone = colunas[2];
+
+      // Transforma as células em campos editáveis
+      nome.innerHTML = `<input type="text" value="${nome.textContent}">`;
+      email.innerHTML = `<input type="email" value="${email.textContent}">`;
+      telefone.innerHTML = `<input type="tel" value="${telefone.textContent}">`;
+
+      botaoEditar.setAttribute("disabled", "disabled");
+      botaoExcluir.setAttribute("disabled", "disabled");
+    }
+  });
+
+  botaoExcluir.addEventListener("click", function () {
+    if (linhaSelecionada) {
+      tabela.deleteRow(linhaSelecionada.rowIndex);
+      linhaSelecionada = null;
+
+      botaoEditar.setAttribute("disabled", "disabled");
+      botaoExcluir.setAttribute("disabled", "disabled");
+    }
+  });
+
+  tabela.addEventListener("blur", function (evento) {
+    if (linhaSelecionada && evento.target.tagName === "INPUT") {
+      const input = evento.target;
+      const celula = input.parentNode;
+      const valor = input.value;
+      celula.textContent = valor;
+
+      linhaSelecionada = null;
+      botaoEditar.removeAttribute("disabled");
+      botaoExcluir.removeAttribute("disabled");
+    }
+  });
+
+  const formulario = document.getElementById("meuFormulario");
+
+  formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+
+    if (linhaSelecionada) {
+      const colunas = linhaSelecionada.cells;
+      colunas[0].textContent = nome;
+      colunas[1].textContent = email;
+      colunas[2].textContent = telefone;
+
+      linhaSelecionada.classList.remove("selecionado");
+      linhaSelecionada = null;
+
+      botaoEditar.setAttribute("disabled", "disabled");
+      botaoExcluir.setAttribute("disabled", "disabled");
+    } else {
+      const novaLinha = tabela.insertRow();
+      const colunaNome = novaLinha.insertCell();
+      const colunaEmail = novaLinha.insertCell();
+      const colunaTelefone = novaLinha.insertCell();
+
+      colunaNome.textContent = nome;
+      colunaEmail.textContent = email;
+      colunaTelefone.textContent = telefone;
+    }
+
+    formulario.reset();
+  });
+});
